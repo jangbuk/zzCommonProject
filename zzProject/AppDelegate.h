ce3164cd5d3af2e4fdc0b070afd49289bad3f22b
zzzili <zzzili@qq.com>
2014-07-31 13:35:27 +0800

    Initial Commit

---
+++ zzProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zzProject
+//
+//  Created by zz on 14-7-31.
+//  Copyright (c) 2014年 YunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zzProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zzProject
+//
+//  Created by zz on 14-7-31.
+//  Copyright (c) 2014年 YunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
